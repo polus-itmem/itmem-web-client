9f25d11cc94a608e024d05c99b733a2e1512eff0
fgs5 <fgs2003@mail.ru>
2022-10-15 01:27:08 +0300

    added an authorisation page
    Raw, needs connection to back-end, needs forward-readressing in case of a correct input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { Button, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Loginner/>
   );
+}
+
+function Loginner() {
+    const navigate = useNavigate();
+    let [error, setError] = useState<boolean>(false);
+
+    function checkLoginAndPassword() {
+        if (false /*some magic*/) {
+            navigate("/user");
+        } else if (false /*some magic*/) {
+            navigate("/dispatcher");
+        } else {
+            setError(true);
+        }
+    }
+
+    if (error) {
+        return (
+            <div className="center">
+                <div className="text">
+                    <h1>Пользователь не найден. Пожалуйста, введите корректную пару логин-пароль.</h1>
+                </div>
+                <div className="input">
+                    <TextField
+                        error
+                        id="outlined-password-input"
+                        label="Login"
+                        type="login"
+                        autoComplete="current-password"
+                    />
+                </div>
+                <div className="input">
+                    <TextField
+                        error
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                    />
+                </div>
+                <div className="button">
+                    <Button onClick={checkLoginAndPassword}>Proceed.</Button>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="center">
+            <div className="text">
+                <h1>Пожалуйста, войдите в систему.</h1>
+            </div>
+            <div className="input">
+                <TextField
+                    id="outlined-password-input"
+                    label="Login"
+                    type="login"
+                    autoComplete="current-password"
+                />
+            </div>
+            <div className="input">
+                <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                />
+            </div>
+            <div className="button">
+                <Button onClick={checkLoginAndPassword}>Proceed.</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
