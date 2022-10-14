067285f997991ecdffa425db7bdf0ddc27f10dfb
LargonG <pan300503@gmail.com>
2022-10-15 01:41:54 +0300

    Fixed solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {routes} from "./data/routes";
+import OrdersPage from "./pages/orders";
+import OrderPage from "./pages/order";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+          <Route path={routes.default} element={<OrdersPage />} />
+          <Route path={routes.orders} element={<OrdersPage/>} />
+          <Route path={routes.order} element={<OrderPage />} />
+      </Routes>
   );
 }
 
