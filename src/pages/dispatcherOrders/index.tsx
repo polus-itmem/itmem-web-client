e1a17da5ee08802e4dc4765429e8232eeb1227ab
fgs5 <fgs2003@mail.ru>
2022-10-15 15:25:37 +0300

    Small fixes
    Resized table, added description

--- src/pages/dispatcherOrders/index.tsx
+++ src/pages/dispatcherOrders/index.tsx
@@ -10,15 +10,14 @@ const columns: GridColDef[] = [
     {field: 'authorId', headerName: 'Customer', width: 130},
     {field: 'date', headerName: 'Execution time', width: 200},
     //{field: 'place', headerName: 'Place', width: 130},
-    {field: 'status', headerName: 'Status', width: 90},
+    {field: 'status', headerName: 'Status', width: 230},
     {
         field: 'description',
         headerName: 'Description',
         width: 1000,
         description: 'This column can`t be sorted due to a meaningless of a sort.',
         sortable: false,
-        valueGetter: (params: GridValueGetterParams) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        valueGetter: (params: GridValueGetterParams) => `${params.row.machinesIds}`,
     },
 ];
 
