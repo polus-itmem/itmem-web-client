44abfab5c123afa3e29385e4704d9d29fb41384e
LargonG <pan300503@gmail.com>
2022-10-15 19:45:21 +0300

    Added list to orders

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -3,31 +3,76 @@ import NavButton from "../../components/nav/navButton";
 import {OrderClip} from "../../components/order-clip";
 import "./styles.css"
 import {Order} from "../../models/core";
+import {DataGrid, GridColDef, GridValueGetterParams} from "@mui/x-data-grid";
+
+class OrderFabric {
+    public constructor() {
+
+    }
+
+    public create(n: number) {
+        let orders: Order[] = [];
+        for (let i = 0; i < n; i++) {
+            orders.push({
+                id: i,
+                date: new Date().toDateString(),
+                car_accept: Math.random() % 2 == 0,
+                car: {
+                    id: i * 100,
+                    park: "центр",
+                    description: "самосвал",
+                    name: `ААА93-${i}`,
+                    number: `${i}${i}${i}`,
+                    driver: {
+                        id: i * 111,
+                        first_name: `Иван-${i}`,
+                        second_name: `Иванович-${i}`
+                    }
+                }
+            })
+        }
+        return orders;
+    }
+}
 
 function OrdersPage() {
     // TODO: читать из базы данных
     // TODO: сделать стили
 
-    let orders: Order[] = [];
+    const columns: GridColDef[] = [
+        {field: 'id', headerName: 'ID', width: 70},
+        {field: 'authorId', headerName: 'Customer', width: 130},
+        {field: 'date', headerName: 'Execution time', width: 200},
+        //{field: 'place', headerName: 'Place', width: 130},
+        {field: 'status', headerName: 'Status', width: 230},
+        {
+            field: 'description',
+            headerName: 'Description',
+            width: 1000,
+            description: 'This column can`t be sorted due to a meaningless of a sort.',
+            sortable: false,
+            valueGetter: (params: GridValueGetterParams) => `${params.row.machinesIds}`,
+        },
+    ];
+
+    let orders: Order[] = new OrderFabric().create(100);
 
     let dates = Array.from(new Set(orders.map(order => order.date)));
 
     console.log(dates);
 
     return (
-        <div className='container'>
+        <div>
             <NavButton className='order-button' link={routes.order}>Заказать</NavButton>
             <NavButton link={routes.default}>Выйти</NavButton>
-            <div className='orders-list'>
-                {dates.map(date => {
-                    return <div className='orders-list-date'>
-                        <div className='orders-list-date-header'>{new Date(date).toDateString()}:</div>
-                        {
-                        orders.filter(order => new Date(order.date).getDay() === new Date(date).getDay()).
-                        map(elem => <OrderClip order={elem}/>)
-                    }
-                    </div>;
-                })}
+            <div style={{height: 880, width: '100%'}}>
+                <DataGrid
+                    rows={orders}
+                    columns={columns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    checkboxSelection
+                />
             </div>
         </div>
     );
